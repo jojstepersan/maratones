6d4913dba168d9d5fd790915e628bfe27a44385b
Stiven <Stiven@DESKTOP-CG1GAOC>
2017-04-13 13:29:40 -0500

    algunos ejercicion del uri parte ii

---
+++ Main/src/main/Main1234.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Stiven
+ */
+public class Main1234 {
+    ArrayList<String> list=new ArrayList<>();
+    Scanner sc=new Scanner(System.in);
+    public static void main(String[] args) {
+        Main1234 m=new Main1234();
+        m.read();
+        m.run();
+    }
+    public void read()
+        {
+        while(sc.hasNext())
+            {
+            list.add(sc.nextLine());
+            }
+        }
+    public void run()
+        {
+            for (int i = 0; i < list.size(); i++) {
+                solve(list.get(i));
+            }
+        }
+    
+    public  static void solve(String str)
+        {
+        String solve="";
+        boolean flag=true;
+        for(int i=0;i<str.length();i++)
+            {
+            if(str.charAt(i)!=' ')
+                {
+                String c=str.charAt(i)+"";    
+                if(flag)
+                    {
+                    solve+=c.toUpperCase();
+                    flag=false;
+                    }
+                else
+                    {
+                    solve+=c.toLowerCase();
+                    flag=true;
+                    }
+                }
+            else
+                solve+=" ";
+            }
+        System.out.println(solve);
+        }
+}
---
+++ Main/src/main/MainEncryp.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Stiven
+ */
+public class MainEncryp {
+    
+    
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc=new Scanner(System.in);
+        char h=('.'-1);
+        System.out.println(h);
+       int a,b;
+       a=sc.nextInt();
+       b=sc.nextInt();
+       double c,d;
+       c=sc.nextDouble();
+       d=c*b;
+       System.out.println("NUMBER = "+a);
+       System.out.print("SALARY = U$ ");
+        System.out.printf("%.2f",d); 
+       System.out.println("");
+    }
+}
