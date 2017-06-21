4fe928d648bef564a2bff5adef8bf00c994fa53b
jojstepersan <jojstepersan@DESKTOP-PLTS099>
2017-06-20 23:49:29 -0500

    uva Celebrity jeopardy

---
+++ Main/src/main/Main_Celebrity_jeopardy.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author jojstepersan
+ */
+public class Main_Celebrity_jeopardy {
+   
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        while(sc.hasNext())
+            System.out.println(sc.nextLine());
+    }
+    
+}
