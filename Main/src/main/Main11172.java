30dd0da4cac0f887548c9dc1925db1e7f4119379
jojstepersan <jojstepersan@DESKTOP-PLTS099>
2017-06-28 17:19:28 -0500

    uva 11172 - Relational Operator

---
+++ Main/src/main/Main11172.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author jojstepersan
+ */
+public class Main11172 {
+    
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int casos=sc.nextInt();
+        for (int i = 0; i < 10; i++) 
+            {
+            long a=sc.nextLong(),b=sc.nextLong();
+            if(a>b)
+                System.out.println(">");
+            else if(a<b)
+                System.out.println("<");
+            else
+                System.out.println("=");
+            }
+    }
+}
