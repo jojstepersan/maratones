696309810d8dcc0978917246e9cfb13fde478c34
jojstepersan <jojstepersan@DESKTOP-PLTS099>
2017-06-28 20:13:53 -0500

    uva 12279	Emoogle Balance

---
+++ Main/src/main/Main12279.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author jojstepersan
+ */
+public class Main12279 {
+    public static void main(String[] args) {
+        int c=1;
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        while(n!=0)
+            {
+            int cnt=0;    
+            for (int i = 0; i < n; i++) 
+                {
+                int num=sc.nextInt();
+                if(num!=0)
+                    cnt++;
+                else
+                    cnt--;
+                }
+            System.out.println("Case "+(c++)+": "+cnt);
+            n=sc.nextInt();
+            }
+    }
+}
