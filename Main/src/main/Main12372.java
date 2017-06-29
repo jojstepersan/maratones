5e40ec75a1e6911f3220c1e260d3b739113e5a55
jojstepersan <jojstepersan@DESKTOP-PLTS099>
2017-06-28 21:18:49 -0500

    uva 12372	Packing for Holiday

---
+++ Main/src/main/Main12372.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author jojstepersan
+ */
+public class Main12372 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int casos=sc.nextInt();
+        int c=1;
+        for (int i = 0; i < casos; i++) {
+            int l,w,h;
+            l=sc.nextInt();
+            w=sc.nextInt();
+            h=sc.nextInt();
+            if(l<=20&&w<=20&&h<=20)
+                System.out.println("Case "+(c++)+": good");
+            else
+                System.out.println("Case "+(c++)+": bad");
+        }
+ 
+    }
+}
