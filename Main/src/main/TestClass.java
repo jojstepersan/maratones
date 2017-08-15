b6cefcc1a60bf1cc49ed34ec017658b132f4e7fc
jojstepersan <jojstepersan@DESKTOP-PLTS099>
2017-08-15 00:30:18 -0500

    test hackerearth

--- Main/src/main/TestClass.java
+++ Main/src/main/TestClass.java
@@ -5,6 +5,7 @@
  */
 package main;
 
+import java.math.BigInteger;
 import java.util.Scanner;
 
 /**
@@ -14,20 +15,49 @@ import java.util.Scanner;
 public class TestClass {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        int row=sc.nextInt();
-        int colums=sc.nextInt();
-        int mat[][]=new int[row][colums];
-        for (int i = 0; i <row; i++) {
-            for (int j = 0; j < colums; j++) {
-                mat[i][j]=sc.nextInt();
+        BigInteger numero=new BigInteger(sc.next());
+        int n[]=new int[10];
+        while(!numero.equals(BigInteger.ZERO))
+            {
+            int m=Integer.valueOf(numero.mod(BigInteger.valueOf(10)).toString());
+            switch (m) {
+                case 0:
+                    n[0]++;
+                    break;
+                case 1:
+                    n[1]++;
+                    break;
+                case 2:
+                    n[2]++;
+                    break;
+                case 3:
+                    n[3]++;
+                    break;
+                case 4:
+                    n[4]++;
+                    break;
+                case 5:
+                    n[5]++;
+                    break;
+                case 6:
+                    n[6]++;
+                    break;
+                case 7:
+                    n[7]++;
+                    break;
+                case 8:
+                    n[8]++;
+                    break;
+                case 9:
+                    n[9]++;
+                    break;
+                default:
+                    break;
             }
-        }
-        for (int i = 0; i < colums; i++) {
-            for (int j = 0; j < row; j++) {
-                System.out.print(mat[j][i]+" ");
+            numero=numero.divide(new BigInteger("10"));
             }
-            System.out.println("");
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i+" "+n[i]);
         }
-        
     }
 }
