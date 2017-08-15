073cb59e25b9b75148e4a94c91f705e815ea61c5
jojstepersan <jojstepersan@DESKTOP-PLTS099>
2017-08-15 00:01:50 -0500

    uva 12554	A Special "Happy Birthday" Song!!!

---
+++ Main/src/main/Main12554.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ *
+ * @author jojstepersan
+ */
+public class Main12554 {
+    public static void main(String[] args) throws IOException {
+        String[] happy={"Happy","birthday","to","you","Happy","birthday","to","you","Happy","birthday","to","Rujia","Happy","birthday","to","you"};
+        String[] people;
+        BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
+        int cases=Integer.valueOf(in.readLine());
+        people=new String[cases];
+        for (int i = 0; i < cases; i++)
+            people[i]=in.readLine();
+        int max;
+        if(happy.length>people.length)
+            max=happy.length;
+        else
+            {
+            int c=1,auxp=people.length;
+            while(auxp>happy.length)
+                {
+                auxp-=happy.length;
+                c++;
+                }
+            max=happy.length*c;
+            }
+        for(int i=0;i<max;i++)
+            System.out.println(people[i%people.length]+": "+happy[i%happy.length]);
+            
+        
+    }
+}
