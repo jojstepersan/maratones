214713346da0fbc9708b45e19013498420f15ebb
jojstepersan <jojstepersan@DESKTOP-PLTS099>
2017-08-15 00:29:23 -0500

    uva 119 - Greedy Gift Givers

--- Main/src/main/Main.java
+++ Main/src/main/Main.java
@@ -5,64 +5,33 @@
  */
 package main;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-
+/**
+ *
+ * @author jojstepersan
+ */
 public class Main {
-Scanner sc=new Scanner(System.in);
-
-public void read()
-    {
-    int c=-1;
-    while(c!=0)
-        {
-        c=sc.nextInt();
-        if(c==0)
-            break;
-        int[] arr=new int[c];
-        for (int i = 0; i < c; i++) 
-           arr[i]=(sc.nextInt());
-        solve(arr);
-        }
-    }
-    public static void main(String[] args) {
-        Main m=new Main();
-        m.read();
-    }
-    public static void solve(int[] arr)
-        {
-        int c=0,p=0;
-        if(arr[0]==0&&arr[0]==arr[arr.length-1])
-            {    
-            p++;    
-            for(int i=1;i<arr.length-1;i++)
+    public static void main(String[] args) throws IOException {
+        BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
+        int casos=Integer.valueOf(in.readLine());
+        String[] friends=new String[casos];
+        int money[]=new int[casos];
+        friends=in.readLine().split(" ");
+        for (int i = 0; i < casos; i++)
+            {
+            StringTokenizer st=new StringTokenizer(in.readLine());
+            st.nextToken();
+            money[i]=Integer.valueOf(st.nextToken());
+            int hm=Integer.valueOf(st.nextToken());
+            money[i]=-money[i]/hm;
+            for (int j = 0; j < hm; j++)
                 {
-                if(arr[i]==0)
-                    c++;
-                else
-                    c=0;
-                if(c==2)
-                    {
-                    p++;
-                    c=0;
-                    }
+                    
                 }
             }
-        else
-            {    
-            for(int i=0;i<arr.length;i++)
-                {
-                if(arr[i]==0)
-                    c++;
-                else
-                    c=0;
-                if(c==2)
-                    {
-                    p++;
-                    c=0;
-                    }
-                }
-            }
-        System.out.println(p);
-        }
+    }
 }
