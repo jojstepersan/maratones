ab560d6f556c46663db4229c7c513f18d4d560b1
stiven <stiven@stiven-PC>
2017-08-26 02:01:44 -0500

    UVA 12478 - Hardest Problem Ever (Easy)

---
+++ Main/src/UVA/Main12478.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package UVA;
+
+/**
+ *
+ * @author stiven
+ * 12478 - Hardest Problem Ever (Easy)
+ */
+public class Main12478 {
+    public static void main(String[] args) {
+        System.out.println("KABIR");
+    }
+}
+/*
+RAKIBUL
+ANINDYA
+MOSHIUR
+SHIPLU
+KABIR
+SUNNY
+OBAIDA
+WASI
+*/
