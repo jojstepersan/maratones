a5afb0861b1c728c32e183ffab302f8241faf5d3
jojstepersan <stiven97.perdomo@gmail.com>
2017-09-21 17:02:30 -0500

    Cops and Robbers

--- Main/src/URI/Main.java
+++ Main/src/URI/Main.java
@@ -21,7 +21,13 @@ public class Main {
     
      boolean isSafe(int maze[][],int x,int y)
         {
-        return (x>=0 && x<N && y>=0 &&  y<N && maze[x][y]==0);
+            /*
+            if (x >= 0 && y >= 0 && x < N  && y < N && maze[x][y] != 0) {
+			return true;
+		}
+		return false;
+            */
+        return (x>=0  && y>=0 &&  x<N && y<N && maze[x][y]!=1);
         }
     
     boolean solveMaze(int Maze[][])
@@ -33,7 +39,7 @@ public class Main {
             {1,1,1,1,1},
             {1,1,1,1,1}
         };
-        if(!solveMazeUtil(Maze,0,0,sol))
+        if(!solveMazeUtil(Maze,0,0,sol,"down"))
             {
             System.out.println("ROBBERS");
             return false;
@@ -42,7 +48,7 @@ public class Main {
         return true;    
         }
 
-    boolean solveMazeUtil(int[][] Maze, int x, int y, int[][] sol) {
+    boolean solveMazeUtil(int[][] Maze, int x, int y, int[][] sol, String direccion) {
         if(x==N-1 && y==N-1)
             {
             sol[x][y]=0;
@@ -51,15 +57,14 @@ public class Main {
         if (isSafe(Maze, x, y))
             {
             sol[x][y]=0;
-            if(solveMazeUtil(Maze, x+1, y, sol))
+            if(!direccion.equals("up")&&solveMazeUtil(Maze, x+1, y, sol,"down"))
                 return true;
-            if(solveMazeUtil(Maze, x-1, y, sol))
+            if (!direccion.equals("left") && solveMazeUtil(Maze,x, y+1, sol,"right"))
                 return true;
-            if (solveMazeUtil(Maze, x, y+1, sol))
+            if(!direccion.equals("right") && solveMazeUtil(Maze, x, y-1, sol,"left"))
                 return true;
-            if(y>0)
-                if(solveMazeUtil(Maze, x, y-1, sol))
-                    return true;
+            if(!direccion.equals("down") && solveMazeUtil(Maze, x-1, y, sol,"up"))
+                return true;
             sol[x][y]=1;
             return false;
             }
