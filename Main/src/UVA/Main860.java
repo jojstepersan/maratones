bebbe5411221f9bcebecbb7e5aa4bfb819ff6c93
jojstepersan <stiven97.perdomo@gmail.com>
2018-10-10 15:48:28 -0500

    860 - Entropy Text Analyzer

--- Main/src/UVA/Main860.java
+++ Main/src/UVA/Main860.java
@@ -8,38 +8,38 @@ package UVA;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.HashMap;
+import java.util.Locale;
 
 /**
  *
- * @author jojstepersan
- * 860 - Entropy Text Analyzer
+ * @author jojstepersan 860 - Entropy Text Analyzer
  */
 public class Main860 {
 
     static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-    static float sum = 0;
+    static double sum = 0;
     static int lmd = 0;
 
     public static void main(String[] args) throws Exception {
         String s;
-
+        Locale.setDefault(Locale.US);
         HashMap<String, Integer> freq = new HashMap<>();
         while (!(s = in.readLine()).equals("****END_OF_INPUT****")) {
             if (!s.equals("****END_OF_TEXT****")) {
                 s = s.toLowerCase();
-                s = s.replace(',', ' ');
-                s = s.replace('.', ' ');
-                s = s.replace(':', ' ');
-                s = s.replace(';', ' ');
-                s = s.replace('!', ' ');
-                s = s.replace('?', ' ');
-                s = s.replace('"', ' ');
-                s = s.replace('(', ' ');
-                s = s.replace(')', ' ');
-                s = s.replace('[', ' ');
-                s = s.replace(']', ' ');
-            //     System.out.println(s);
-                String arr[] = s.split(" ");
+//                s = s.replace(',', ' ');
+//                s = s.replace('.', ' ');
+//                s = s.replace(':', ' ');
+//                s = s.replace(';', ' ');
+//                s = s.replace('!', ' ');
+//                s = s.replace('?', ' ');
+//                s = s.replace('"', ' ');
+//                s = s.replace('(', ' ');
+//                s = s.replace(')', ' ');
+//                s = s.replace('[', ' ');
+//                s = s.replace(']', ' ');
+                //     System.out.println(s);
+                String arr[] = s.split("[, .:;!?()\"]");
                 for (int i = 0; i < arr.length; i++) {
                     if (!arr[i].equals("")) {
                         lmd++;
@@ -51,13 +51,16 @@ public class Main860 {
                     }
                 }
             } else {
-
-                freq.forEach((k, v) -> {
-                    sum += v * (Math.log10(lmd) - Math.log10(v));
-                });
-                float er = sum / lmd;
-                Float res=(float)er*100/(float)Math.log10(lmd);
-                System.out.printf("%d %.1f %d\n", lmd, er,(int)Math.round(res));
+                if (lmd != 0) {
+                    freq.forEach((k, v) -> {
+                        sum += v * (Math.log10(lmd) - Math.log10(v));
+                    });
+                    double er = sum / lmd;
+                    double res =  er * 100 /  Math.log10(lmd);
+                    System.out.printf("%d %.1f %d\n", lmd, er, (int)Math.round(res));
+                } else {
+                    System.out.println("0 0.0 0");
+                }
                 lmd = 0;
                 sum = 0;
                 freq = new HashMap<>();
