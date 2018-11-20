8aa022eada40e1ec6410161d9238ae42f1269724
Unknown <jojhans.perdomos@konradlorenz.edu.co>
2018-11-20 10:59:57 -0500

    uri 2914

--- Main/src/URI/Main2914.java
+++ Main/src/URI/Main2914.java
@@ -8,6 +8,8 @@ package URI;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.StringTokenizer;
 
 /**
@@ -16,13 +18,16 @@ import java.util.StringTokenizer;
  */
 public class Main2914 {
 
-    static int MAX = 100000;
+    static int MAX = 0;//100000;
     static ArrayList<Integer> primes = new ArrayList<>();
     static boolean sieve[] = new boolean[MAX + 5];
     static ArrayList<Integer> primeFactors = new ArrayList<>();
     static int maxPrime[] = new int[MAX + 5];
+    static List<In> input=new ArrayList<>();
 
     public static void calculatePrimes() {
+        maxPrime = new int[MAX + 5];
+        sieve = new boolean[MAX + 5];
         sieve[0] = sieve[1] = true;
         int i;
         for (i = 2; i * i <= MAX; i++) {
@@ -58,30 +63,57 @@ public class Main2914 {
     }
 
     public static void main(String[] args) throws Exception {
-        calculatePrimes();
-        for (int i = 2; i <= MAX; i++) {
-            calculatePrimeFactors(i);
-        }
+        
+//        for (int i = 2; i <= MAX; i++) {
+//            calculatePrimeFactors(i);
+//        }
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.valueOf(in.readLine().trim());
         for (int i = 0; i < t; i++) {
-            int n, k;
+           int n, k;
             StringTokenizer st = new StringTokenizer(in.readLine().trim());
             n = Integer.valueOf(st.nextToken());
             k = Integer.valueOf(st.nextToken());
-            System.out.println(solve(n, k));
+            MAX=Math.max(MAX, n);
+            input.add(new In(n, k));
+            
+            //System.out.println(solve(n, k));
+        }
+        //MAX=Collections.max(input).n;
+        calculatePrimes();
+        for (In x : input) {
+            System.out.println(solve(x.n, x.k));
         }
     }
 
     public static int solve(int n, int k) {
         int c = 0;
         for (int i = 2; i <= n; i++) {
+            if (maxPrime[i] == 0) {
+                calculatePrimeFactors(i);
+            }
             if (maxPrime[i] <= k) {
                 c++;
             }
         }
         return c;
     }
+}
+
+class In implements Comparable<In> {
+
+    Integer n, k;
+
+    public In(Integer n, Integer k) {
+        this.n = n;
+        this.k = k;
+    }
+
+    @Override
+    public int compareTo(In o) {
+        return this.n.compareTo(o.n);
+    }
+
 }
 /*
 4
