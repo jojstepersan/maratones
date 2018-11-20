61203158562bbeb28799d762b479ecfcbada5628
Unknown <jojhans.perdomos@konradlorenz.edu.co>
2018-11-20 09:42:47 -0500

    uri 2914

--- Main/src/URI/Main2914.java
+++ Main/src/URI/Main2914.java
@@ -8,9 +8,6 @@ package URI;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
 import java.util.StringTokenizer;
 
 /**
@@ -19,12 +16,12 @@ import java.util.StringTokenizer;
  */
 public class Main2914 {
 
-    static int MAX = 10000;
+    static int MAX = 100000;
     static ArrayList<Integer> primes = new ArrayList<>();
     static boolean sieve[] = new boolean[MAX + 5];
     static ArrayList<Integer> primeFactors = new ArrayList<>();
-    static int maxPrime[]=new int[MAX+5];
-    
+    static int maxPrime[] = new int[MAX + 5];
+
     public static void calculatePrimes() {
         sieve[0] = sieve[1] = true;
         int i;
@@ -50,25 +47,23 @@ public class Main2914 {
         for (int i = 0; (long) primes.get(i) * primes.get(i) <= value; ++i) {
             factor = primes.get(i);
             while (temp % factor == 0) {
-                primeFactors.add( factor);
+                primeFactors.add(factor);
                 temp /= factor;
             }
         }
         if (temp != 1) {
             primeFactors.add(temp);
         }
-        maxPrime[value]=primeFactors.get(primeFactors.size()-1);
+        maxPrime[value] = primeFactors.get(primeFactors.size() - 1);
     }
 
     public static void main(String[] args) throws Exception {
         calculatePrimes();
-        for(int i=2;i<=MAX;i++){
+        for (int i = 2; i <= MAX; i++) {
             calculatePrimeFactors(i);
         }
-       // System.out.println(Arrays.toString(maxPrime));
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-      //Scanner in=new Scanner(System.in);
-      int t = Integer.valueOf(in.readLine().trim());
+        int t = Integer.valueOf(in.readLine().trim());
         for (int i = 0; i < t; i++) {
             int n, k;
             StringTokenizer st = new StringTokenizer(in.readLine().trim());
@@ -81,8 +76,9 @@ public class Main2914 {
     public static int solve(int n, int k) {
         int c = 0;
         for (int i = 2; i <= n; i++) {
-            if(maxPrime[i]<=k)
+            if (maxPrime[i] <= k) {
                 c++;
+            }
         }
         return c;
     }
@@ -93,4 +89,4 @@ public class Main2914 {
 10 4
 15 3
 5 20
-*/
+ */
