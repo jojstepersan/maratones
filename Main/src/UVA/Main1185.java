6d2db76acb67d2d0b12fbcbb8658897484f5b19f
jojstepersan <stiven97.perdomo@gmail.com>
2019-04-18 10:57:28 -0500

    1185 - Big Number

---
+++ Main/src/UVA/Main1185.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package UVA;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+/**
+ *
+ * @author jojstepersan 1185 - Big Number
+ */
+public class Main {
+
+    static int MAX = 10000000 + 5;
+    static int DP[] = new int[MAX];
+
+    static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws Exception {
+
+        double last = 0;
+	for(int i = 1; i <= 10000000; i++) {
+            last += Math.log10(i);
+            DP[i] = (int)last;
+	}
+        int x=Integer.valueOf(in.readLine().trim());
+        for (int i = 0; i <x; i++) {
+            int n=Integer.valueOf(in.readLine().trim());   
+            System.out.println(DP[n]+1);
+        }
+    
+    }
+
+}
