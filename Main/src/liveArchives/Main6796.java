ab888426e11ebc7972700fc2b044ef4a5e541181
jojstepersan <stiven97.perdomo@gmail.com>
2019-04-22 23:06:29 -0500

    6796 - Factorial Factors with set

--- Main/src/liveArchives/Main6796.java
+++ Main/src/liveArchives/Main6796.java
@@ -8,11 +8,14 @@ package liveArchives;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
-
+import java.util.Set;
+import java.util.SortedSet;
 
 /**
- *6796 - Factorial Factors
+ * 6796 - Factorial Factors
+ *
  * @author jojstepersan
  */
 public class Main6796 {
@@ -20,11 +23,11 @@ public class Main6796 {
     static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
     static int MAX = 150000;
     static List<Integer> primes = new ArrayList<>();
-    static boolean sieve[] = new boolean[MAX+5];
+    static boolean sieve[] = new boolean[MAX + 5];
     static List<Long> primesFactors = new ArrayList<>();
-    static List<Long> primesFactorDistincs = new ArrayList<>();
-    static int DPP[] = new int[MAX+5];
-    static int DPPD[] = new int[MAX+5];
+    static Set<Long> primesFactorDistincs = new HashSet<Long>();
+    static int DPP[] = new int[MAX + 5];
+    static int DPPD[] = new int[MAX + 5];
 
     static void calculatePrime() {
         sieve[0] = sieve[1] = true;
@@ -46,7 +49,7 @@ public class Main6796 {
 
     static void calculatePrimeFactors(int value) {
         primesFactors.clear();
-      //  primesFactorDistincs.clear();
+        //  primesFactorDistincs.clear();
         long temp = value;
         int factor;
         for (int i = 0; (long) primes.get(i) * primes.get(i) <= value; ++i) {
@@ -54,17 +57,17 @@ public class Main6796 {
             while (temp % factor == 0) {
                 primesFactors.add((long) factor);
                 Long auxFactor = (long) factor;
-                if (!primesFactorDistincs.contains(auxFactor)) {
-                    primesFactorDistincs.add(auxFactor);
-                }
+                //  if (!primesFactorDistincs.contains(auxFactor)) {
+                primesFactorDistincs.add(auxFactor);
+                //}
                 temp /= factor;
             }
         }
         if (temp != 1) {
             primesFactors.add(temp);
-            if (!primesFactorDistincs.contains(temp)) {
-                primesFactorDistincs.add(temp);
-            }
+            //if (!primesFactorDistincs.contains(temp)) {
+            primesFactorDistincs.add(temp);
+            //}
         }
     }
 
@@ -73,13 +76,13 @@ public class Main6796 {
         for (int i = 2; i < MAX; i++) {
             calculatePrimeFactors(i);
             DPP[i] = DPP[i - 1] + primesFactors.size();
-            DPPD[i] =  primesFactorDistincs.size();
+            DPPD[i] = primesFactorDistincs.size();
         }
         int c = Integer.valueOf(in.readLine().trim());
 
         for (int i = 0; i < c; i++) {
             int n = Integer.valueOf(in.readLine().trim());
-            System.out.println(DPPD[n]+" "+ DPP[n]);
+            System.out.println(DPPD[n] + " " + DPP[n]);
         }
     }
 
